27f4dbc95ae395e4035163629f992b58c1ba2b11
Aziz Souabni <57048839+azizswebni@users.noreply.github.com>
2024-12-10 13:52:11 +0000

    adjust train model step

--- steps/model_train.py
+++ steps/model_train.py
@@ -13,7 +13,6 @@ from sklearn.base import RegressorMixin
 from zenml import step
 from zenml.client import Client
 
-from .config import ModelNameConfig
 
 experiment_tracker = Client().active_stack.experiment_tracker
 
@@ -24,7 +23,6 @@ def train_model(
     x_test: pd.DataFrame,
     y_train: pd.Series,
     y_test: pd.Series,
-    config: ModelNameConfig,
 ) -> RegressorMixin:
     """
     Args:
@@ -36,31 +34,13 @@ def train_model(
         model: RegressorMixin
     """
     try:
-        model = None
-        tuner = None
 
-        if config.model_name == "lightgbm":
-            mlflow.lightgbm.autolog()
-            model = LightGBMModel()
-        elif config.model_name == "randomforest":
-            mlflow.sklearn.autolog()
-            model = RandomForestModel()
-        elif config.model_name == "xgboost":
-            mlflow.xgboost.autolog()
-            model = XGBoostModel()
-        elif config.model_name == "linear_regression":
-            mlflow.sklearn.autolog()
-            model = LinearRegressionModel()
-        else:
-            raise ValueError("Model name not supported")
+        mlflow.lightgbm.autolog()
+        
+        model = LightGBMModel()
+        
+        trained_model = model.train(x_train, y_train)
 
-        tuner = HyperparameterTuner(model, x_train, y_train, x_test, y_test)
-
-        if config.fine_tuning:
-            best_params = tuner.optimize()
-            trained_model = model.train(x_train, y_train, **best_params)
-        else:
-            trained_model = model.train(x_train, y_train)
         return trained_model
     except Exception as e:
         logging.error(e)
